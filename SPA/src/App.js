590a8b0cc26acfefb7da1304d475b2ab8c9a69ae
richman_stewart <kiwipxl@gmail.com>
2020-11-26 15:04:16 +1100

    add bootstrap and add template video card

--- SPA/src/App.js
+++ SPA/src/App.js
@@ -1,28 +1,39 @@
 import React from 'react';
 import './App.css';
+import Card from 'react-bootstrap/Card';
 
-function Button(props) {
-  const [count, setCount] = React.useState(0);
-
-  function onClick() {
-    setCount(count + 1);
-  }
+function VideosGrid(props) {
 
   return (
-    <div>
-      <p>you've clicked {count} times</p>
+    <VideoCard title="How large can a bacteria get? Life & Size 3" numViews={100}>
 
-      <button onClick={() => onClick()}>
-        {props.text}
-      </button>
-    </div>
+    </VideoCard>
+  )
+}
+
+function VideoCard(props) {
+
+  console.log(props);
+
+  return (
+    <Card>
+      <Card.Img variant="top" src="holder.js/100px180"/>
+
+      <Card.Body>
+        <Card.Title>{props.title}</Card.Title>
+
+        <Card.Text>
+          {props.numViews.toString()}
+        </Card.Text>
+      </Card.Body>
+    </Card>
   );
 }
 
 function App() {
   return (
     <div className="App">
-      <Button text="clickedy clcik"></Button>
+      <VideosGrid></VideosGrid>
     </div>
   );
 }
