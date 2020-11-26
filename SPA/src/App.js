fdd8afc1750a4c32711d0c54050874d22f3eb0af
richman_stewart <kiwipxl@gmail.com>
2020-11-26 20:34:54 +1100

    styled video list. lookin pretty decent!

--- SPA/src/App.js
+++ SPA/src/App.js
@@ -5,25 +5,87 @@ import Card from 'react-bootstrap/Card';
 function VideosGrid(props) {
 
   return (
-    <VideoCard title="How large can a bacteria get? Life & Size 3" numViews={100}>
+    <div className="video-grid">
+      {/* <div className="video-card-container">
+        <div className="video-card"></div>
+      </div>
+      <div className="video-card-container">
+        <div className="video-card"></div>
+      </div> */}
 
-    </VideoCard>
+      <div className="video-grid-card-container">
+        <VideoCard
+          title="How large can a bacteria get? Life & Size 3"
+          numViews={100}
+          thumbnail="https://i.ytimg.com/vi/wbR-5mHI6bo/maxresdefault.jpg"
+        >
+
+        </VideoCard>
+      </div>
+
+      <div className="video-grid-card-container">
+        <VideoCard
+          title="How large can a bacteria get? Life & Size 3"
+          numViews={100}
+          thumbnail="https://i.ytimg.com/vi/wbR-5mHI6bo/maxresdefault.jpg"
+        >
+
+        </VideoCard>
+      </div>
+
+      <div className="video-grid-card-container">
+        <VideoCard
+          title="How large can a bacteria get? Life & Size 3"
+          numViews={100}
+          thumbnail="https://i.ytimg.com/vi/wbR-5mHI6bo/maxresdefault.jpg"
+        >
+
+        </VideoCard>
+      </div>
+
+      <div className="video-grid-card-container">
+        <VideoCard
+          title="How large can a bacteria get? Life & Size 3"
+          numViews={100}
+          thumbnail="https://i.ytimg.com/vi/wbR-5mHI6bo/maxresdefault.jpg"
+        >
+
+        </VideoCard>
+      </div>
+
+      <div className="video-grid-card-container">
+        <VideoCard
+          title="How large can a bacteria get? Life & Size 3"
+          numViews={100}
+          thumbnail="https://i.ytimg.com/vi/wbR-5mHI6bo/maxresdefault.jpg"
+        >
+
+        </VideoCard>
+      </div>
+
+      <div className="video-grid-card-container">
+        <VideoCard
+          title="How large can a bacteria get? Life & Size 3"
+          numViews={100}
+          thumbnail="https://i.ytimg.com/vi/wbR-5mHI6bo/maxresdefault.jpg"
+        >
+
+        </VideoCard>
+      </div>
+    </div>
   )
 }
 
 function VideoCard(props) {
-
-  console.log(props);
-
   return (
-    <Card>
-      <Card.Img variant="top" src="holder.js/100px180"/>
+    <Card className="video-card">
+      <Card.Img className="video-card-img" variant="top" src={props.thumbnail}/>
 
       <Card.Body>
-        <Card.Title>{props.title}</Card.Title>
+        <Card.Title className="video-card-title">{props.title}</Card.Title>
 
-        <Card.Text>
-          {props.numViews.toString()}
+        <Card.Text className="video-card-info">
+          {"1.3M views | 1 month ago"}
         </Card.Text>
       </Card.Body>
     </Card>
@@ -33,7 +95,9 @@ function VideoCard(props) {
 function App() {
   return (
     <div className="App">
-      <VideosGrid></VideosGrid>
+      <div className="video-grid-container">
+        <VideosGrid></VideosGrid>
+      </div>
     </div>
   );
 }
