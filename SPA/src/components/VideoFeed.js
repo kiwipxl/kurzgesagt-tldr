08dcdf7a809285dfb0cd11a511213687210d3641
richman_stewart <kiwipxl@gmail.com>
2020-11-27 13:32:06 +1100

    detect when to load more videos into feed

--- SPA/src/components/VideoFeed.js
+++ SPA/src/components/VideoFeed.js
@@ -4,6 +4,8 @@ import VideoCard from './VideoCard';
 export default (props) => {
   const [items, setItems] = React.useState([]);
 
+  const rootEl = React.useRef(null);
+
   React.useEffect(() => {
     fetch('http://localhost:7800')
       .then(res => res.json())
@@ -11,8 +13,24 @@ export default (props) => {
       .catch(err => console.error(err));
   }, []);
 
+  function onScroll(ev) {
+    const rootElBottom = rootEl.current.getClientRects()[0].bottom;
+
+    if (rootElBottom <= window.innerHeight) {
+      console.log('load more!');
+    }
+  }
+
+  React.useEffect(() => {
+    window.addEventListener('scroll', onScroll);
+
+    return () => {
+      window.removeEventListener('scroll', onScroll);
+    };
+  }, []);
+
   return (
-    <div className="video-feed">
+    <div ref={rootEl} className="video-feed">
       {items.map(video => (
         <div key={video.id} className="video-feed-card-container">
           <VideoCard
