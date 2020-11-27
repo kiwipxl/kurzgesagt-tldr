9893df431c0ec5143d93ad4f011632080b1bbd31
richman_stewart <kiwipxl@gmail.com>
2020-11-27 12:23:39 +1100

    formatting for number of video views

--- SPA/src/components/VideoCard.js
+++ SPA/src/components/VideoCard.js
@@ -1,6 +1,17 @@
 import Card from 'react-bootstrap/Card';
 
 export default (props) => {
+    let viewsShortHand;
+    if (props.numViews >= 1000000) {
+        viewsShortHand = (Math.round(props.numViews / 1000000 * 10) / 10).toString() + "M";
+    }else if (props.numViews >= 100000) {
+        viewsShortHand = Math.round(props.numViews / 1000).toString() + "K";
+    }else if (props.numViews >= 1000) {
+        viewsShortHand = (Math.round(props.numViews / 1000 * 10) / 10).toString() + "K";
+    }else {
+        viewsShortHand = props.numViews.toString();
+    }
+
     return (
         <Card className="video-card">
         <Card.Img className="video-card-img" variant="top" src={props.thumbnail}/>
@@ -9,7 +20,7 @@ export default (props) => {
             <Card.Title className="video-card-title">{props.title}</Card.Title>
 
             <Card.Text className="video-card-info">
-            {"1.3M views | 1 month ago"}
+            {`${viewsShortHand} views | 1 month ago`}
             </Card.Text>
         </Card.Body>
         </Card>
