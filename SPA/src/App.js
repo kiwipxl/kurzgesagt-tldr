8b7e5b5538e41c565a8417407508cc2879048279
richman_stewart <kiwipxl@gmail.com>
2020-12-07 20:53:10 +1100

    add github pages deploy option

--- SPA/src/App.js
+++ SPA/src/App.js
@@ -53,7 +53,7 @@ function App() {
 
   return (
     <div className="App">
-      <BrowserRouter>
+      <BrowserRouter basename='/kurzgesagt-tldr'>
         <Header showBack={showBackButton}>
           <AppSwitch setHeaderOptions={setHeaderOptions}/>
         </Header>
